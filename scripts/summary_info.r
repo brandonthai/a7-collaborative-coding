fe68f740b17771a3f8ab8690fc3bd83bd38f6478
Kevin Ly <kevinly7@uw.edu>
2016-02-22 13:48:09 -0800

    created data directory

--- scripts/summary_info.r
+++ scripts/summary_info.r
@@ -1,8 +1,22 @@
 require('dplyr')
 
+data <- read.csv('../data/intro_survey_data.csv', stringsAsFactors = FALSE)
+
+
+
+numOfResponse <- nrow(data)
+numOfFreshman <- nrow(filter(data, What.is.your.current.class.standing. == 'Freshman'))
+
 # A function that takes in a dataset and returns a list of info about it:
 info_function <- function(dataset) {
   ret <- list()
+  ret$numOfResponse <- nrow(dataset)
+  ret$numOfFreshman
+  ret$numOfSophomore
+  ret$numOfJunior
+  ret$numOfSenior
+  ret$numOfMac
+  ret$numoOfWindow
   
   return (ret)
 }
