21b9b3fc399610e470ea2790a9e530d3b7556c69
Kevin Ly <kevinly7@uw.edu>
2016-02-22 14:25:38 -0800

    summary script completed

--- scripts/summary_info.r
+++ scripts/summary_info.r
@@ -5,14 +5,18 @@ data <- read.csv('../data/intro_survey_data.csv', stringsAsFactors = FALSE)
 names(data) <- c('ClassStanding', 'InfoProspective', 'OS', 'TerminalExperience', 'GitFamiliarity', 'MarkDownFamiliarity', 'RFamiliarity', 'ProgrammingExperience', 'CountriesVisited', 'CatOrDog', 'SeahawksFan')
 
 numOfResponse <- nrow(data)
-
 numOfFreshman <- nrow(filter(data, ClassStanding == "Freshman"))
 numOfSophomore <- nrow(filter(data, ClassStanding == "Sophomore"))
 numOfJunior <- nrow(filter(data, ClassStanding == "Junior"))
 numOfSenior <- nrow(filter(data, ClassStanding == "Senior"))
-numOfMac <- nrow(filter(data, OS == 'Mac'))
-numofWindows <- nrow(filter(data, OS == 'Windows'))
-macApplyToInfo <- filter(data, OS == 'Mac' && InfoProspective == 'Yes')
+OSApplyToInfo <- data %>%
+                  group_by(OS) %>%
+                  filter(InfoProspective == 'Yes') %>%
+                  summarise(InfoProspectives = n())
+GredeApplyToInfo <- data %>%
+                  group_by(ClassStanding) %>%
+                  filter(InfoProspective == 'Yes') %>%
+                  summarise(InfoProspectives = n())
 
 # A function that takes in a dataset and returns a list of info about it:
 info_function <- function(dataset) {
@@ -24,6 +28,13 @@ info_function <- function(dataset) {
   ret$numOfSenior <- nrow(filter(dataset, ClassStanding == "Senior"))
   ret$numOfMac <- nrow(filter(dataset, OS == 'Mac'))
   ret$numoOfWindows <- nrow(filter(dataset, OS == 'Windows'))
-  
+  ret$OSApplyToInfo <- dataset %>%
+                    group_by(OS) %>%
+                    filter(InfoProspective == 'Yes') %>%
+                    summarise(InfoProspectives = n())
+  ret$GredeApplyToInfo <- dataset %>%
+                    group_by(ClassStanding) %>%
+                    filter(InfoProspective == 'Yes') %>%
+                    summarise(InfoProspectives = n())
   return (ret)
 }
