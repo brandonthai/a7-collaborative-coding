00cdcf5688d369c56ee664b67e8d7c168868e87f
Amy Wang <amyewang115@gmail.com>
2016-02-22 15:07:03 -0800

    added comments to chart 1 script

--- scripts/chart_1.R
+++ scripts/chart_1.R
@@ -1,26 +1,31 @@
 build_chart_1 <- function(data) {
+  
+  # changes the names of the columns in original data
   names(data) <- c('ClassStanding', 'InfoProspective', 'OS', 'TerminalExperience', 'GitFamiliarity', 
                    'MarkDownFamiliarity', 'RFamiliarity', 'ProgrammingExperience', 'CountriesVisited', 
                    'CatOrDog', 'SeahawksFan')
   
+  # create new data frame that summarizes the number of students with different git experience
   git_familiarity_data <- data %>% group_by(GitFamiliarity) %>%
       summarise(number_of_students = n())
   
+  # name for the x axis
   x <- list(
     title = "Git Familiarity"
   )
   
-  # Setting y axis title 
+  
+  # name for the y axis
   y <- list(
     title = "Number of Students"
   )
   
-  # Creating a bar chart
+  # creating a bar chart comparing the number of students with what level of git familiarity 
   p <- plot_ly(git_familiarity_data,
               x = GitFamiliarity,
               y = number_of_students,
               type = "bar")
-    
+  # setting the layout (x axis, y axis, title)
   layout(p, xaxis = x, yaxis = y, title = 'Familiarity with Git')
   
   return(p)
