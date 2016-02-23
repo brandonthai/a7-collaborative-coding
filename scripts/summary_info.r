ed8bb01fbd4b4dafe37408b1ac01cec6ecb1d2a8
baronbaura <bbaura@uw.edu>
2016-02-22 16:02:21 -0800

    working on chart

--- scripts/summary_info.r
+++ scripts/summary_info.r
@@ -1,39 +1,21 @@
 require('dplyr')
 
-data <- read.csv('../data/intro_survey_data.csv', stringsAsFactors = FALSE)
-
-
-
-numOfResponse <- nrow(data)
-numOfFreshman <- nrow(filter(data, ClassStanding == "Freshman"))
-numOfSophomore <- nrow(filter(data, ClassStanding == "Sophomore"))
-numOfJunior <- nrow(filter(data, ClassStanding == "Junior"))
-numOfSenior <- nrow(filter(data, ClassStanding == "Senior"))
-OSApplyToInfo <- data %>%
-                  group_by(OS) %>%
-                  filter(InfoProspective == 'Yes') %>%
-                  summarise(InfoProspectives = n())
-GradeApplyToInfo <- data %>%
-                  group_by(ClassStanding) %>%
-                  filter(InfoProspective == 'Yes') %>%
-                  summarise(InfoProspectives = n())
-
 # A function that takes in a dataset and returns a list of info about it:
 info_function <- function(dataset) {
-  names(data) <- c('ClassStanding', 'InfoProspective', 'OS', 'TerminalExperience', 'GitFamiliarity', 'MarkDownFamiliarity', 'RFamiliarity', 'ProgrammingExperience', 'CountriesVisited', 'CatOrDog', 'SeahawksFan')
   ret <- list()
+  names(dataset) <- c('ClassStanding', 'InfoProspective', 'OS', 'TerminalExperience', 'GitFamiliarity', 'MarkDownFamiliarity', 'RFamiliarity', 'ProgrammingExperience', 'CountriesVisited', 'CatOrDog', 'SeahawksFan')
   ret$numOfResponse <- nrow(dataset)
   ret$numOfFreshman <- nrow(filter(dataset, ClassStanding == "Freshman"))
   ret$numOfSophomore <- nrow(filter(dataset, ClassStanding == "Sophomore"))
   ret$numOfJunior <- nrow(filter(dataset, ClassStanding == "Junior"))
   ret$numOfSenior <- nrow(filter(dataset, ClassStanding == "Senior"))
   ret$numOfMac <- nrow(filter(dataset, OS == 'Mac'))
-  ret$numoOfWindows <- nrow(filter(dataset, OS == 'Windows'))
+  ret$numOfWindows <- nrow(filter(dataset, OS == 'Windows'))
   ret$OSApplyToInfo <- dataset %>%
                     group_by(OS) %>%
                     filter(InfoProspective == 'Yes') %>%
                     summarise(InfoProspectives = n())
-  ret$GredeApplyToInfo <- dataset %>%
+  ret$GradeApplyToInfo <- dataset %>%
                     group_by(ClassStanding) %>%
                     filter(InfoProspective == 'Yes') %>%
                     summarise(InfoProspectives = n())
